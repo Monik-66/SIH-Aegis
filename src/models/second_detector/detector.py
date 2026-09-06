fc912072df5ba1b9eea4baf24005784348ea240f
Monik-66 <monik389885@gmail.com>
2026-09-06 17:58:16 +0000

    Add second detector interface

---
+++ src/models/second_detector/detector.py
@@ -0,0 +1,12 @@
+class SecondDetector:
+
+    def predict(self, audio, sample_rate):
+        """
+        Run the second spoof detector.
+
+        This is an interface placeholder.
+        The actual model will be connected later.
+        """
+        raise NotImplementedError(
+            "Second detector is not implemented yet."
+        )
